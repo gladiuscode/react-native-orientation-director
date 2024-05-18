7e472ea574acb3d17b9cd4059c249c67a0150eaa
gladiuscode <gladiusservices@protonmail.com>
2024-05-18 21:54:05 +0200

    chore(docs): leave note in InterfaceOrientation enum

--- src/types/InterfaceOrientation.enum.ts
+++ src/types/InterfaceOrientation.enum.ts
@@ -1,7 +1,13 @@
 export enum InterfaceOrientation {
   unknown = 0,
   portrait = 1,
+
+  /*
+    Note: On IOS, the portraitUpsideDown is not supported by all iPhone devices
+    that haven't got a home button.
+   */
   portraitUpsideDown = 2,
+
   landscapeLeft = 3,
   landscapeRight = 4,
 }
