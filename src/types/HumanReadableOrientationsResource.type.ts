b5edf829230ce6fcdde4e5378dd0f288626fc4ea
gladius <gladiusservices@protonmail.com>
2024-09-14 10:23:19 +0200

    feat(js): implement convertAutoRotationToHumanReadableString

---
+++ src/types/HumanReadableOrientationsResource.type.ts
@@ -0,0 +1,3 @@
+import type { Orientation } from './Orientation.enum';
+
+export type HumanReadableOrientationsResource = Record<Orientation, string>;
--- src/types/InterfaceOrientationToLocalizedStringProvider.type.ts
+++
@@ -1,6 +0,0 @@
-import type { Orientation } from './Orientation.enum';
-
-export type InterfaceOrientationToLocalizedStringProvider = Record<
-  Orientation,
-  string
->;
