7f96437e730291d87cbe3333eed14715db2ceb35
gladius <gladiusservices@protonmail.com>
2024-10-12 18:23:58 +0200

    feat(android): implement logic to match face up and face down orientations

--- android/src/main/java/com/orientationdirector/implementation/SensorListener.kt
+++ android/src/main/java/com/orientationdirector/implementation/SensorListener.kt
@@ -7,6 +7,7 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.util.Log
 import com.facebook.react.bridge.ReactApplicationContext
+import kotlin.math.PI
 
 class SensorListener(
   context: ReactApplicationContext,
@@ -17,6 +18,11 @@ class SensorListener(
   private var mMagneticFieldSensor: Sensor?
 
   private var hasRequiredSensors: Boolean
+
+  private val accelerometerReading = FloatArray(3)
+  private val magnetometerReading = FloatArray(3)
+  private val rotationMatrix = FloatArray(9)
+  private val orientationAngles = FloatArray(3)
 
   private var lastRotationDetected: Int? = null
   private var onOrientationChangedCallback: ((orientation: Int) -> Unit)? = null
@@ -48,7 +54,40 @@ class SensorListener(
       return
     }
 
-//    Log.d(TAG, "onSensorChanged $event")
+    if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+      System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+    } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+      System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+    }
+
+    updateOrientationAngles()
+
+    val z = orientationAngles[0]
+    val x = orientationAngles[1]
+    val y = orientationAngles[2]
+
+    val orientation = when {
+
+      // Portrait (Rotation_0)
+      x.equals(0f) && y.equals(-0f) && z.equals(0f) -> "Face Up"
+      x.equals(0f) && y.equals(-PI.toFloat()) && z.equals(PI.toFloat()) -> "Face Down"
+
+      // Landscape Right (Rotation_90)
+      x.equals(-0f) && y.equals(-0f) && z.equals(PI.div(2).toFloat()) -> "Face Up"
+      x.equals(-0f) && y.equals(-PI.toFloat()) && z.equals(PI.div(2).toFloat()) -> "Face Down"
+
+      // Portrait Upside Down (Rotation_180)
+      x.equals(0f) && y.equals(-0f) && z.equals(PI.toFloat()) -> "Face Up"
+      x.equals(0f) && y.equals(-PI.toFloat()) && z.equals(-0f) -> "Face Down"
+
+      // Landscape Left (Rotation_270)
+      x.equals(-0f) && y.equals(-0f) && z.equals(-PI.div(2).toFloat()) -> "Face Up"
+      x.equals(-0f) && y.equals(-PI.toFloat()) && z.equals(-PI.div(2).toFloat()) -> "Face Down"
+
+      else -> "Unknown Orientation"
+    }
+
+    Log.d(TAG, orientation)
   }
 
   override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -72,6 +111,17 @@ class SensorListener(
     Log.d(TAG, "disable - started")
     mSensorManager.unregisterListener(this)
     Log.d(TAG, "disable - done")
+  }
+
+  private fun updateOrientationAngles() {
+    SensorManager.getRotationMatrix(
+      rotationMatrix,
+      null,
+      accelerometerReading,
+      magnetometerReading
+    )
+
+    SensorManager.getOrientation(rotationMatrix, orientationAngles)
   }
 
   companion object {
