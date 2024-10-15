e228ac31c2064b9000cd55c50cbb7a0f75005498
gladius <gladiusservices@protonmail.com>
2024-10-15 22:35:10 +0200

    feat(android): add junit and create basic class to test it out

---
+++ android/src/test/java/com/orientationdirector/implementation/OrientationDirectorModuleImplTest.kt
@@ -0,0 +1,14 @@
+package com.orientationdirector.implementation
+
+import com.facebook.react.bridge.ReactApplicationContext
+import org.junit.Assert
+import org.junit.Test
+
+class OrientationDirectorModuleImpl {
+
+  @Test
+  fun getInterfaceOrientation(): Unit {
+    Assert.assertEquals(6, 7)
+  }
+
+}
