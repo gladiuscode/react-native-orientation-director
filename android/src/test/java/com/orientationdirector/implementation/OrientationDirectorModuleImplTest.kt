30b69aa0a792a11cfa5d2cbf304a467349f64a0e
gladius <gladiusservices@protonmail.com>
2024-10-17 00:10:52 +0200

    feat(android): implement robolectric and androidx.test.core to get fake context and run first test

--- android/src/test/java/com/orientationdirector/implementation/OrientationDirectorModuleImplTest.kt
+++ android/src/test/java/com/orientationdirector/implementation/OrientationDirectorModuleImplTest.kt
@@ -1,13 +1,12 @@
 package com.orientationdirector.implementation
 
-import com.facebook.react.bridge.ReactApplicationContext
 import org.junit.Assert
 import org.junit.Test
 
 class OrientationDirectorModuleImpl {
 
   @Test
-  fun getInterfaceOrientation(): Unit {
+  fun getInterfaceOrientation() {
     Assert.assertEquals(6, 7)
   }
 
