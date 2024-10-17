c0fbfdc28a5a8f14b59c085384c0ac12cf9365b4
gladius <gladiusservices@protonmail.com>
2024-10-17 23:40:25 +0200

    chore(android): clean test code structure

--- android/src/test/java/com/orientationdirector/implementation/Utils.kt
+++ android/src/test/java/com/orientationdirector/implementation/Utils.kt
@@ -9,7 +9,6 @@ import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class UtilsTest {
-
   private var context = BridgeReactContext(ApplicationProvider.getApplicationContext())
 
   private var mUtils = Utils(context)
@@ -17,10 +16,7 @@ class UtilsTest {
   @Test
   fun assert_device_orientation_is_portrait() {
     val orientationAngles = FloatArray(3)
-
-    // pitch radians
     orientationAngles[1] = -(Math.PI / 2).toFloat()
-    // roll radians
     orientationAngles[2] = -0f
 
     val orientation = mUtils.convertToDeviceOrientationFrom(orientationAngles)
@@ -34,9 +30,7 @@ class UtilsTest {
 
   @Test
   fun assert_device_orientation_is_landscape_right() {
-
     val orientationAngles = FloatArray(3)
-
     orientationAngles[1] = 0f
     orientationAngles[2] = (Math.PI / 2).toFloat()
 
@@ -51,9 +45,7 @@ class UtilsTest {
 
   @Test
   fun assert_device_orientation_is_portrait_upside_down() {
-
     val orientationAngles = FloatArray(3)
-
     orientationAngles[1] = (Math.PI / 2).toFloat()
     orientationAngles[2] = 0f
 
@@ -67,9 +59,7 @@ class UtilsTest {
 
   @Test
   fun assert_device_orientation_is_landscape_left() {
-
     val orientationAngles = FloatArray(3)
-
     orientationAngles[1] = 0f
     orientationAngles[2] = -(Math.PI / 2).toFloat()
 
@@ -84,9 +74,7 @@ class UtilsTest {
 
   @Test
   fun assert_device_orientation_is_face_down() {
-
     val orientationAngles = FloatArray(3)
-
     orientationAngles[1] = 0f
     orientationAngles[2] = -(Math.PI).toFloat()
 
@@ -101,9 +89,7 @@ class UtilsTest {
 
   @Test
   fun assert_device_orientation_is_face_up() {
-
     val orientationAngles = FloatArray(3)
-
     orientationAngles[1] = 0f
     orientationAngles[2] = -0f
 
@@ -115,5 +101,4 @@ class UtilsTest {
       orientation
     )
   }
-
 }
