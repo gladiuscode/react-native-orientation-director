562c393b8d518cccb642a4dff76328a0139518f4
gladius <gladiusservices@protonmail.com>
2024-10-20 18:23:16 +0200

    chore(android): delete useless tests

--- android/src/test/java/com/orientationdirector/implementation/OrientationDirectorModuleImplTest.kt
+++ android/src/test/java/com/orientationdirector/implementation/OrientationDirectorModuleImplTest.kt
@@ -101,23 +101,6 @@ class OrientationDirectorModuleImplTest {
   }
 
   @Test
-  fun assert_auto_rotation_enabled_at_startup() {
-    // TODO: Find a way to set the value of Settings.System.ACCELEROMETER_ROTATION if possible
-  }
-
-  @Test
-  fun assert_lock_orientation_to_portrait() {
-    mModule.lockTo(1)
-
-    // TODO: This test fails because currentActivity is null, check if we can mock it out and how
-//    assertEquals(
-//      "",
-//      ActivityInfo.SCREEN_ORIENTATION_PORTRAIT,
-//      context.currentActivity?.requestedOrientation
-//    )
-  }
-
-  @Test
   fun assert_interface_orientation_matches_locked_to_portrait() {
     mModule.lockTo(1)
 
