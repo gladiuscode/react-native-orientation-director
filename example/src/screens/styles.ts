ce4b8f5c23b5484bf1bbcd65efb23608c4dc91a1
gladius <gladiusservices@protonmail.com>
2024-11-01 16:21:36 +0100

    feat: move some old app features into homepage

---
+++ example/src/screens/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const homepageStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  marginBottom: {
+    marginBottom: 12,
+  },
+  buttonsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
