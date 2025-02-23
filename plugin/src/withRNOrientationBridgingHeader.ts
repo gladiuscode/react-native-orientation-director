6d99b12f8e6c7a7f04619f3f4275f099a6f67972
gladius <gladiusservices@protonmail.com>
2025-02-23 21:28:22 +0100

    feat: add swift template check in withRNOrientationBridgingHeader

--- plugin/src/withRNOrientationBridgingHeader.ts
+++ plugin/src/withRNOrientationBridgingHeader.ts
@@ -1,21 +1,28 @@
-import { ConfigPlugin, ExportedConfigWithProps } from '@expo/config-plugins';
-import { withAppBridgingHeader } from './custom-mod/withBridgingHeader';
-import { AppDelegateProjectFile } from '@expo/config-plugins/build/ios/Paths';
+import {ConfigPlugin, ExportedConfigWithProps, IOSConfig} from "@expo/config-plugins";
+import {withAppBridgingHeader} from "./custom-mod/withBridgingHeader";
+import {AppDelegateProjectFile} from "@expo/config-plugins/build/ios/Paths";
 
 export const withRNOrientationBridgingHeader: ConfigPlugin = (config) => {
   return withAppBridgingHeader(config, readBridgingHeaderFileAndUpdateContents);
-};
+}
 
-async function readBridgingHeaderFileAndUpdateContents(
-  config: ExportedConfigWithProps<AppDelegateProjectFile>
-) {
-  const { modResults: bridgingHeaderFile } = config;
+async function readBridgingHeaderFileAndUpdateContents(config: ExportedConfigWithProps<AppDelegateProjectFile>) {
+  const {projectRoot} = config.modRequest;
+  if (isObjCTemplate(projectRoot)) {
+    return config;
+  }
+
+  const {modResults: bridgingHeaderFile} = config;
 
   const libraryHeaderImport = '#import "OrientationDirector.h"';
 
-  bridgingHeaderFile.contents = bridgingHeaderFile.contents.concat(
-    `\n${libraryHeaderImport}`
-  );
+  bridgingHeaderFile.contents = bridgingHeaderFile.contents.concat(`\n${libraryHeaderImport}`);
 
   return config;
 }
+
+function isObjCTemplate(projectRoot: string) {
+  const appDelegateFile = IOSConfig.Paths.getAppDelegate(projectRoot);
+  return appDelegateFile.language !== 'swift';
+}
+
