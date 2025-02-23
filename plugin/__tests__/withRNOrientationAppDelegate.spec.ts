fbc724a4f13bf90e04d7a5ca10b1239aeb9fa104
gladius <gladiusservices@protonmail.com>
2025-02-23 23:11:30 +0100

    feat(tests): create withRNOrientationAppDelegate.spec

---
+++ plugin/__tests__/withRNOrientationAppDelegate.spec.ts
@@ -0,0 +1,31 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import {
+  objCFileUpdater,
+  swiftFileUpdater,
+} from '../src/withRNOrientationAppDelegate';
+
+describe('withRNOrientationAppDelegate', function () {
+  beforeEach(function () {
+    jest.resetAllMocks();
+  });
+
+  it('updates the AppDelegate.mm with both header import and method implementation', async function () {
+    const appDelegatePath = path.join(__dirname, './fixtures/AppDelegate.mm');
+    const appDelegate = await fs.promises.readFile(appDelegatePath, 'utf-8');
+
+    const result = objCFileUpdater(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+
+  it('updates the AppDelegate.swift with the method implementation', async function () {
+    const appDelegatePath = path.join(
+      __dirname,
+      './fixtures/AppDelegate.swift'
+    );
+    const appDelegate = await fs.promises.readFile(appDelegatePath, 'utf-8');
+
+    const result = swiftFileUpdater(appDelegate);
+    expect(result).toMatchSnapshot();
+  });
+});
--- plugin/src/withRNOrientationAppDelegate.ts
+++ plugin/src/withRNOrientationAppDelegate.ts
@@ -42,7 +42,7 @@ export function swiftFileUpdater(originalContents: string): string {
   const supportedInterfaceOrientationsForCodeBlock = `override func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
   return OrientationDirector.getSupportedInterfaceOrientationsForWindow()
 }\n`;
-  const rightBeforeLastClosingBrace = /\}[^}]*$/g;
+  const rightBeforeLastClosingBrace = /}[^}]*$/g;
 
   const results = mergeContents({
     tag: '@react-native-orientation-director/supportedInterfaceOrientationsFor-implementation',
