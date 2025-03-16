bb0b86a4b459857e3f9541b3a25eae4cd0273f18
gladius <gladiusservices@protonmail.com>
2025-03-16 13:15:13 +0100

    chore(android): properly comment the ConfigurationChangedBroadcastReceiver

--- android/src/main/java/com/orientationdirector/implementation/ConfigurationChangedBroadcastReceiver.kt
+++ android/src/main/java/com/orientationdirector/implementation/ConfigurationChangedBroadcastReceiver.kt
@@ -7,6 +7,13 @@ import android.content.IntentFilter
 import android.os.Build
 import com.facebook.react.bridge.ReactApplicationContext
 
+/**
+ * This custom broadcast receiver is needed to properly update the interface orientation when
+ * the user has disabled the automatic rotation.
+ *
+ * It listens for an explicit intent that the MainActivity can send in the onConfigurationChanged
+ * method and calls a custom callback that is set in the main implementation init
+ */
 class ConfigurationChangedBroadcastReceiver internal constructor(private val context: ReactApplicationContext) :
   BroadcastReceiver() {
 
@@ -20,6 +27,11 @@ class ConfigurationChangedBroadcastReceiver internal constructor(private val con
     onReceiveCallback = callback
   }
 
+  /**
+   * This method registers the receiver by checking the api we are currently running with.
+   * With the latest changes in Android 14, we need to explicitly set the `Context.RECEIVER_NOT_EXPORTED`
+   * flag.
+   */
   fun register() {
     val filter = IntentFilter("${context.packageName}.CONFIGURATION_CHANGED")
 
