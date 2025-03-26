3fc723bab8b2440d979f01264da4df4eb2b8afbd
gladius <gladiusservices@protonmail.com>
2025-03-26 19:45:58 +0100

    feat(plugin): implement intent and configuration imports logic

--- plugin/__tests__/withRNOrientationMainActivity.spec.ts
+++ plugin/__tests__/withRNOrientationMainActivity.spec.ts
@@ -8,8 +8,30 @@ describe('withRNOrientationMainActivity', function () {
     jest.resetAllMocks();
   });
 
-  it('updates the MainActivity.kt with both import and method implementation', async function () {
+  it('updates the MainActivity.kt with both imports and method implementation', async function () {
     const mainActivityPath = path.join(__dirname, './fixtures/MainActivity.kt');
+    const mainActivity = await fs.promises.readFile(mainActivityPath, 'utf-8');
+
+    const result = ktFileUpdater(mainActivity);
+    expect(result).toMatchSnapshot();
+  });
+
+  it("skips the MainActivity.kt intent import when it's already set", async function () {
+    const mainActivityPath = path.join(
+      __dirname,
+      './fixtures/MainActivityWithIntentImport.kt'
+    );
+    const mainActivity = await fs.promises.readFile(mainActivityPath, 'utf-8');
+
+    const result = ktFileUpdater(mainActivity);
+    expect(result).toMatchSnapshot();
+  });
+
+  it("skips the MainActivity.kt configuration import when it's already set", async function () {
+    const mainActivityPath = path.join(
+      __dirname,
+      './fixtures/MainActivityWithConfigurationImport.kt'
+    );
     const mainActivity = await fs.promises.readFile(mainActivityPath, 'utf-8');
 
     const result = ktFileUpdater(mainActivity);
