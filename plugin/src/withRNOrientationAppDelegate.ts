f43314f7e0dabc0fc0705c943c618c0d63c1193d
gladiuscode <gladiusservices@protonmail.com>
2025-06-28 18:09:46 +0200

    feat: extend logic to accept sdkVersion

--- plugin/src/withRNOrientationAppDelegate.ts
+++ plugin/src/withRNOrientationAppDelegate.ts
@@ -15,22 +15,39 @@ async function readAppDelegateFileAndUpdateContents(
 ): Promise<ExportedConfigWithProps<AppDelegateProjectFile>> {
   const { modResults: appDelegateFile } = config;
 
-  const worker = getCompatibleFileUpdater(appDelegateFile.language);
-  appDelegateFile.contents = worker(appDelegateFile.contents);
+  const fileUpdater = getCompatibleFileUpdater(appDelegateFile.language);
+  if (fileUpdater.language === 'swift') {
+    const { worker } = fileUpdater;
+    appDelegateFile.contents = worker(
+      appDelegateFile.contents,
+      config.sdkVersion
+    );
+  } else {
+    const { worker } = fileUpdater;
+    appDelegateFile.contents = worker(appDelegateFile.contents);
+  }
 
   return config;
 }
 
 function getCompatibleFileUpdater(
   language: AppDelegateProjectFile['language']
-): (originalContents: string) => string {
+):
+  | { language: 'swift'; worker: typeof swiftFileUpdater }
+  | { language: 'objc' | 'objcpp'; worker: typeof objCFileUpdater } {
   switch (language) {
     case 'objc':
     case 'objcpp': {
-      return objCFileUpdater;
+      return {
+        language,
+        worker: objCFileUpdater,
+      };
     }
     case 'swift':
-      return swiftFileUpdater;
+      return {
+        language,
+        worker: swiftFileUpdater,
+      };
     default:
       throw new Error(
         `Cannot add React Native Orientation Director code to AppDelegate of language "${language}"`
@@ -38,7 +55,7 @@ function getCompatibleFileUpdater(
   }
 }
 
-export function swiftFileUpdater(originalContents: string): string {
+export function swiftFileUpdater(originalContents: string, _?: string): string {
   const supportedInterfaceOrientationsForCodeBlock = `\n  override func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
     return OrientationDirector.getSupportedInterfaceOrientationsForWindow()
   }\n`;
